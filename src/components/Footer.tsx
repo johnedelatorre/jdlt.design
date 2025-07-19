d0967cb5873e23e89ffcea7d74ef99441cb1dfca
John De La Torre-Ugarte <johndelatorre-ugarte@Johns-MacBook-Pro.local>
2025-07-19 19:58:43 +0200

    Add elegant fade-in animations and semi-transparent styling to footer tooltips

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -78,14 +78,16 @@ const Footer: React.FC = () => {
                         <FontAwesomeIcon icon={link.icon} className="text-sm" />
                       </button>
                       
-                      {/* Custom Tooltip */}
-                      {isHovered && (
-                        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-white text-gray-900 text-sm rounded-lg shadow-lg whitespace-nowrap z-10">
-                          {link.tooltip}
-                          {/* Tooltip arrow */}
-                          <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-white"></div>
-                        </div>
-                      )}
+                      {/* Elegant Custom Tooltip */}
+                      <div className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-3 px-3 py-2 bg-white/95 backdrop-blur-sm text-gray-800 text-sm font-medium rounded-lg shadow-xl border border-gray-200/50 whitespace-nowrap z-10 transition-all duration-200 ease-out ${
+                        isHovered 
+                          ? 'opacity-100 translate-y-0 scale-100' 
+                          : 'opacity-0 translate-y-1 scale-95 pointer-events-none'
+                      }`}>
+                        {link.tooltip}
+                        {/* Elegant arrow */}
+                        <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-white/95"></div>
+                      </div>
                     </div>
                   );
                 } else {
@@ -103,14 +105,16 @@ const Footer: React.FC = () => {
                         <FontAwesomeIcon icon={link.icon} className="text-sm" />
                       </a>
                       
-                      {/* Custom Tooltip */}
-                      {isHovered && (
-                        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-white text-gray-900 text-sm rounded-lg shadow-lg whitespace-nowrap z-10">
-                          {link.tooltip}
-                          {/* Tooltip arrow */}
-                          <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-white"></div>
-                        </div>
-                      )}
+                      {/* Elegant Custom Tooltip */}
+                      <div className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-3 px-3 py-2 bg-white/95 backdrop-blur-sm text-gray-800 text-sm font-medium rounded-lg shadow-xl border border-gray-200/50 whitespace-nowrap z-10 transition-all duration-200 ease-out ${
+                        isHovered 
+                          ? 'opacity-100 translate-y-0 scale-100' 
+                          : 'opacity-0 translate-y-1 scale-95 pointer-events-none'
+                      }`}>
+                        {link.tooltip}
+                        {/* Elegant arrow */}
+                        <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-white/95"></div>
+                      </div>
                     </div>
                   );
                 }
